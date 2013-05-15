00e07c9e289d1a8ae9a7522aecf159dbf06eaa89
kalo-glb <kalo.glb@gmail.com>
2013-05-15 15:54:07 +0300

    Fix project

---
+++ King-Survival-5/TestKingSurvival/TestKing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using KingSurvivalGame;
+
+namespace TestKingSurvival
+{
+    [TestFixture]
+    class TestKing
+    {
+        [Test]
+        public void TestSomeMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
